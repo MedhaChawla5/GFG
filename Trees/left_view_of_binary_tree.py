d7cb1a6aeccc7a465debd22b518b7798b6ad535a
MedhaChawla5 <133873481+MedhaChawla5@users.noreply.github.com>
2025-03-27 22:36:07 +0530

    Create left_view_of_binary_tree.py

---
+++ Trees/left_view_of_binary_tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def LeftView(self, root):
+        if root is None:
+            return []
+        q = []
+        r = []
+        q.append(root)
+        currl = 0
+        
+        while(q):
+            qlen = len(q)
+            r.append([])
+            
+            for i in range(qlen):
+                node = q.pop(0)
+                r[currl].append(node.data)
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+            currl += 1
+        res = []
+        for i in range(len(r)):
+            res.append(r[i][0])
+        return res
