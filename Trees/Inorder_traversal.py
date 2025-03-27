d5493235e23a4920f83f3017c59ed2a418794ec1
MedhaChawla5 <133873481+MedhaChawla5@users.noreply.github.com>
2025-03-27 19:00:24 +0530

    Create Inorder_traversal.py

---
+++ Trees/Inorder_traversal.py
@@ -0,0 +1,7 @@
+class Solution:
+    def inOrder(self,root):
+        if root is None:
+            return []
+        return self.inOrder(root.left) + [root.data] + self.inOrder(root.right)
+        # code here
+
